91f6c3884cbb83da8b5d3ab90de85e6994d726ea
Jake Elder <jake@jakeelder.info>
2023-03-19 15:29:46 +0700

    tweak: Remove debug

--- apps/app/src/api.ts
+++ apps/app/src/api.ts
@@ -4,9 +4,7 @@ import { Property } from "@rems/types";
 const get = {
   async properties(): Promise<Property[]> {
     const res = await fetch(`${process.env.API_URL}/properties`);
-    const properties = await res.text();
-    console.log(properties);
-    return [];
+    return res.json();
   }
 };
 
