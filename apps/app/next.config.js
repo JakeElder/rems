9ab88ae8eb4cceed5d91432a3d5d4f9d02afc8be
Jake Elder <jake@jakeelder.info>
2023-05-01 11:12:11 +0700

    fix: image remote patterns

--- apps/app/next.config.js
+++ apps/app/next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { hostname: "127.0.0.1" },
-      { hostname: "https://cms.stage.jyo.mindfulstudio.io/" }
+      { hostname: "cms.stage.jyo.mindfulstudio.io" }
     ]
   }
 };
