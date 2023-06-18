1e8317591b38112b3d14db3ab1276c687d544842
Jake Elder <jake@jakeelder.info>
2023-06-19 06:45:31 +0700

    feat: add FilterBar

--- apps/ui/src/Components/FilterBar/FilterBar.tsx
+++ apps/ui/src/Components/FilterBar/FilterBar.tsx
@@ -1,33 +1,77 @@
-import React from "react";
+import React, { MutableRefObject, useRef } from "react";
 import css from "./FilterBar.module.css";
 import Container from "../../Elements/Container/Container";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSliders, faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { useDraggable } from "react-use-draggable-scroll";
 
 type Props = {};
 
 const FilterBar = ({ }: Props) => {
   const toggles = [
     "Sea View",
-    "Ocean View",
-    "Balcony",
-    "Garden",
-    "Fitness Center / Gym",
-    "Elevator",
+    "Water View",
+    "Pool",
     "Terrace",
-    "Office",
-    "Jaccuzzi",
-    "Swimming Pool",
-    "Gym",
-    "Sauna",
-    "New Built",
+    "Garden",
+    "Balcony",
+    "Mountain View",
+    "Air Conditioning",
+    "Fitness Center / Gym",
+    "Garage",
     "Bar",
-    "Panoramic",
-    "Scenic View"
+    "Modern",
+    "Fireplace",
+    "Panoramic / Scenic View",
+    "Privacy",
+    "Elevator",
+    "Sauna",
+    "Jacuzzi",
+    "Gated Community",
+    "New Built",
+    "Cinema",
+    "Investment Property",
+    "Open Kitchen",
+    "Office",
+    "Outdoor Kitchen",
+    "Wine Cellar",
+    "Game Room",
+    "Renovated",
+    "Beachfront",
+    "Indoor Pool",
+    "Coastal",
+    "Golf View",
+    "Tennis Court",
+    "Ocean View",
+    "Duplex",
+    "Mansion",
+    "Steam Room",
+    "Waterfront",
+    "Seafront",
+    "Hilltop",
+    "Boat House",
+    "Library",
+    "City View",
+    "Oceanfront",
+    "Equestrian",
+    "High Altitude",
+    "Lake View",
+    "Helipad",
+    "Private Beach",
+    "Vineyard / Winery",
+    "River View",
+    "Lakefront",
+    "Riverfront",
+    "Private Airport"
   ];
 
+  const $sections = useRef<HTMLDivElement>(null);
+  const { events } = useDraggable(
+    $sections as MutableRefObject<HTMLDivElement>
+  );
+
   return (
-    <div className={css["root"]}>
+    <div className={css["root"]} ref={$sections} {...events}>
       <Container full>
         <div className={css["sections"]}>
           <div className={css["filters"]}>
