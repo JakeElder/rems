5d15584238158f0070841f84ab60765c758d003e
Jake Elder <jake@jakeelder.info>
2023-06-20 18:16:28 +0700

    fix: type

--- apps/app/src/app/real-estate/[id]/page.tsx
+++ apps/app/src/app/real-estate/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { RealEstatePage } from "@rems/ui/src";
 import api from "../../../api";
 
-export default async function Home({ params }) {
+export default async function Home({ params }: any) {
   const property = await api.get.property(params.id.split("-").slice(-1));
   return <RealEstatePage property={property} />;
 }
