6553a735162543c6f5acbeb2441e53eeb2b4ff5b
Jake Elder <jake@jakeelder.info>
2023-06-25 06:39:01 +0700

    fix: font loading

--- apps/ui/src/Utils/FontLoader.tsx
+++ apps/ui/src/Utils/FontLoader.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { PropsWithChildren } from "react";
+import cn from "classnames";
 import { Inter, Libre_Baskerville } from "@next/font/google";
 
 const inter = Inter({
@@ -18,7 +19,7 @@ type Props = PropsWithChildren<{}>;
 
 const FontLoader = ({ children }: Props) => {
   return (
-    <div>
+    <div className={cn(inter.variable, libre.variable)}>
       <style jsx global>{`
         :root {
           --font-inter: ${inter.style.fontFamily};
