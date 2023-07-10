0d2a282fe9a427c1458af388e724af1598db788c
Jake Elder <jake@jakeelder.info>
2023-07-10 14:12:02 +0700

    fix: heroku config

--- apps/cms/config/database.ts
+++ apps/cms/config/database.ts
@@ -29,12 +29,9 @@ export default ({ env }) => {
     postgres: {
       connection: {
         connectionString: env("DATABASE_URL"),
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 3306),
-        database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
-        ssl: env.bool("DATABASE_SSL", false),
+        ssl: env.bool("DATABASE_SSL", false) && {
+          rejectUnauthorized: false
+        },
         schema: env("DATABASE_SCHEMA", "public")
       },
       pool: {
