2029e358215effe82e04ce9e23a379b3b14b70b6
Jake Elder <jake@jakeelder.info>
2023-08-02 21:40:05 +0700

    feat: fun

--- apps/api/src/app/properties/route.ts
+++ apps/api/src/app/properties/route.ts
@@ -1,12 +1,12 @@
 import { NextResponse } from "next/server";
 import qs from "qs";
 import { ServerRealEstateQuerySchema } from "@rems/schemas";
-// import resolve from "./resolve";
+import resolve from "./resolve";
 
 export async function GET(req: Request) {
   const url = new URL(req.url);
   const params = qs.parse(url.search.substring(1));
   const query = ServerRealEstateQuerySchema.parse(params);
-  // return NextResponse.json(await resolve(query));
-  return NextResponse.json(query);
+  const properties = await resolve(query);
+  return NextResponse.json(properties);
 }
