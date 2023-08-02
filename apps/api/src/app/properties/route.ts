130ab61897f060f2e37ca3b6a103e010d5135e22
Jake Elder <jake@jakeelder.info>
2023-08-02 19:19:35 +0700

    fix: no edge

--- apps/api/src/app/properties/route.ts
+++ apps/api/src/app/properties/route.ts
@@ -3,8 +3,6 @@ import qs from "qs";
 import { ServerRealEstateQuerySchema } from "@rems/schemas";
 import resolve from "./resolve";
 
-export const runtime = "edge";
-
 export async function GET(req: Request) {
   const url = new URL(req.url);
   const params = qs.parse(url.search.substring(1));
