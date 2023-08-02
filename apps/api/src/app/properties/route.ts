436ebb68bd4dcd3b03ee9e0e5f3aed73c7c79aa3
Jake Elder <jake@jakeelder.info>
2023-08-02 19:18:38 +0700

    feat: edge

--- apps/api/src/app/properties/route.ts
+++ apps/api/src/app/properties/route.ts
@@ -3,6 +3,8 @@ import qs from "qs";
 import { ServerRealEstateQuerySchema } from "@rems/schemas";
 import resolve from "./resolve";
 
+export const runtime = "edge";
+
 export async function GET(req: Request) {
   const url = new URL(req.url);
   const params = qs.parse(url.search.substring(1));
