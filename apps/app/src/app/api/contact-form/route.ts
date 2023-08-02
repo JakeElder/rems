de2d862bf5fd82518984a6b9239888549fa63fb5
Jake Elder <jake@jakeelder.info>
2023-08-02 17:12:03 +0700

    fix: api build

--- apps/app/src/app/api/contact-form/route.ts
+++ apps/app/src/app/api/contact-form/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import sendMail from "../../../utils";
+import { sendMail } from "../../actions";
 import { ContactFormSchema } from "@rems/schemas/src/contact-form";
 
 export async function POST(req: Request) {
