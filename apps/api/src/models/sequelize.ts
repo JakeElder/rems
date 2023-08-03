5b1c7a608050b2fbb2ddb82dc9039e16cdefd14d
Jake Elder <jake@jakeelder.info>
2023-08-03 21:28:15 +0700

    feat: build stuff..

--- apps/api/src/models/sequelize.ts
+++ apps/api/src/models/sequelize.ts
@@ -1,12 +1,17 @@
 import { Sequelize } from "sequelize";
 import * as pg from "pg";
 
+const dialectOptions = {
+  ssl: process.env.DATABASE_SSL
+    ? {
+      rejectUnauthorized: true,
+      ca: process.env.DATABASE_CA_CERT
+    }
+    : null
+};
+
 export default new Sequelize(process.env.DATABASE_URL!, {
   dialectModule: pg,
-  dialectOptions: {
-    ssl: process.env.DATABASE_SSL
-      ? { require: true, rejectUnauthorized: false }
-      : null
-  },
+  dialectOptions,
   logging: false
 });
